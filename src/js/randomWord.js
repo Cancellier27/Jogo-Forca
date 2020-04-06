8883105d3a30b3e3f6a56c379426cc3d6b3d7943
Cancellier27 <filipecancelliercosta@gmail.com>
2020-04-06 17:19:08 +0100

    Created a file to get a random word

---
+++ src/js/randomWord.js
@@ -0,0 +1,25 @@
+const predeterminedWords = [
+    'CACHORRO',
+    'BRASIL',
+    'PARALELEPIPEDO',
+    'CELULAR',
+    'AUSTRALIA',
+    'OCEANO',
+    'DINOSSARO',
+    'POKEMON',
+    'ARVORE',
+    'CARRO',
+    'BICICLETA',
+    'ARMARIO',
+    'LUNETA',
+    'BARRACA',
+    'RESTAURANTE',
+    'PRATO',
+    'ESTRADA',
+    'CASTELO'
+]
+
+const predeterminedWordsLength = predeterminedWords.length
+let num = Math.floor(Math.random() * predeterminedWordsLength)
+
+
